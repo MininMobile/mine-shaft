99ac6aa790253ed9ebab61ff8c5d8e61f828997c
MininMobile <miningamer@gmail.com>
2018-02-04 12:38:51 +0000

    Better Jumping

--- Assets/PlayerBehavior.cs
+++ Assets/PlayerBehavior.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PlayerBehavior : MonoBehaviour {
     
     public float Speed = 1f;
-    
+
+    private float _jumpingVel = 0f;
     private bool _jumpingUp = false;
 
 	void Update()
     {
+        // Get Input
         if (Input.GetAxis("Horizontal") != 0)
             this.transform.Translate(Input.GetAxis("Horizontal") * Speed, 0, 0);
 
@@ -17,5 +19,19 @@ public class PlayerBehavior : MonoBehaviour {
             this.transform.Translate(0, 0, Input.GetAxis("Vertical") * Speed);
 
         if (Input.GetKeyUp(KeyCode.Space)) { _jumpingUp = true; }
+
+        // Jumping
+        if (_jumpingUp && _jumpingVel > 0)
+        {
+            if (_jumpingVel < 5)
+                _jumpingVel++;
+            
+            if (_jumpingVel > 5)
+                _jumpingVel--;
+
+            _jumpingUp = false;
+        }
+
+        this.transform.Translate(0, _jumpingVel, 0);
     }
 }
