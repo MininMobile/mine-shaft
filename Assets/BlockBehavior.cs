54d2a591e03dcdaba3b6c57ba8b2fa088b397b12
MininMobile <miningamer@gmail.com>
2018-02-04 11:47:21 +0000

    Block Behavior

--- Assets/BlockBehavior.cs
+++ Assets/BlockBehavior.cs
@@ -2,15 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlockBehavior : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+public class BlockBehavior : MonoBehaviour
+{
+    
+	void Start ()
+    {
+        BlockUpdate();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    static void BlockUpdate()
+    {
+
+    }
 }
