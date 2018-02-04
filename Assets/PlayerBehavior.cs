c80649b7dec28a8d3f2c733b27b84cb325519bc9
MininMobile <miningamer@gmail.com>
2018-02-04 12:33:01 +0000

    Compact Code
    Improve Jumping 50%

--- Assets/PlayerBehavior.cs
+++ Assets/PlayerBehavior.cs
@@ -3,24 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour {
-
+    
     public float Speed = 1f;
+    
+    private bool _jumpingUp = false;
 
 	void Update()
     {
         if (Input.GetAxis("Horizontal") != 0)
-        {
             this.transform.Translate(Input.GetAxis("Horizontal") * Speed, 0, 0);
-        }
 
         if (Input.GetAxis("Vertical") != 0)
-        {
             this.transform.Translate(0, 0, Input.GetAxis("Vertical") * Speed);
-        }
 
-        if (Input.GetKeyUp(KeyCode.Space))
-        {
-            this.transform.Translate(0, Speed, 0);
-        }
+        if (Input.GetKeyUp(KeyCode.Space)) { _jumpingUp = true; }
     }
 }
