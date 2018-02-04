b82a741e5523f500d6c72484d0225083db452ec3
MininMobile <miningamer@gmail.com>
2018-02-04 20:54:12 +0000

    Create Face Behavior

---
+++ Assets/BlockBehavior_faces.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockBehavior_faces : MonoBehaviour
+{
+    MeshRenderer renderer;
+
+    void Start()
+    {
+        renderer = this.transform.GetComponent<MeshRenderer>();
+    }
+
+    void OnCollisionEnter(Collision collision) { renderer.enabled = false; }
+    void OnCollisionExit(Collision collision) { renderer.enabled = true; }
+}
